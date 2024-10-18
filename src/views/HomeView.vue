24fd72b3f51de8ebc6dc2de06cfd98d5882ca254
Gabo <gabriel.gonzalez@linktic.com>
2024-10-18 15:46:24 -0400

    primer commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+
+    <label for="tarea" class="task"> Tarea</label>
+    <input type="text" class="add" id="tarea" placeholder="Agrega una tarea" />
+  </main>
+</template>
